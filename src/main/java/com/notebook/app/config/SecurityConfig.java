1c3499d19ad47f33c04464b7034c14d607050763
RajkumarSony <rk.sony4848@gmail.com>
2025-03-12 01:44:29 +0900

    Enhanced the code with InMemory Authentication

--- src/main/java/com/notebook/app/config/SecurityConfig.java
+++ src/main/java/com/notebook/app/config/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,24 +17,34 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        // Allow access to contact page and public resources and deny access to admin page for all requests by default
-        http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/contact").permitAll()        // Allow access to contact page
-                        .requestMatchers("/public/**").permitAll()      // Allow access to public resources
-                        .requestMatchers("/admin").denyAll()            // Deny access to admin page
-                        .anyRequest().authenticated());
-
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
-
-        // Disable session creation by default in Spring Security 5 to prevent session fixation vulnerabilities and improve security
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        //        http.formLogin(Customizer.withDefaults());
-
-        // Enable basic authentication for all requests
+        // http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                        .password("{noop}password1")
+                        .roles("USER")
+                        .build()
+            );
+        }
+
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build()
+            );
+        }
+        return manager;
     }
 }
